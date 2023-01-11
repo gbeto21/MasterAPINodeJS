dd151019e19b3cbda0c3780d5cf61320dc78eae5
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-11 09:13:09 -0600

    The server was initialized.

--- api-rest-node/index.js
+++ api-rest-node/index.js
@@ -1,3 +1,16 @@
 const {connection} = require("./database/connection")
+const cors = require("cors")
+const express = require("express")
 
-connection()
+const PORT = 3900
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.listen(PORT, ()=>{
+  console.log(`Server listening at port: ${PORT}`);
+  connection()
+})
