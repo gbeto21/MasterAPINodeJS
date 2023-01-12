4324c6d2cb2a35d6306f203881b417df30df6888
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-12 09:14:06 -0600

    The server was initialized.

--- api-rest-social-network/index.js
+++ api-rest-social-network/index.js
@@ -1,3 +1,17 @@
 const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
+
+const PORT = 3900;
+const app = express();
 
 connection();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`✅ Server listen at port: ${PORT}`);
+});
