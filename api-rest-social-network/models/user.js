7e1595cbd69dff85fae684bb12a26194262122e4
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-12 09:47:18 -0600

    The controllers and the user model were created

---
+++ api-rest-social-network/models/user.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  name: { type: String, required: true },
+  surname: String,
+  nick: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    requred: true,
+  },
+  role: {
+    type: String,
+    default: "role_user",
+  },
+  image: {
+    type: "String",
+    default: "default.png",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = model("User", UserSchema, "users");
