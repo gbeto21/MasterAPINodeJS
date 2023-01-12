e926deea62be352705dbc2160379b5024cdf7213
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-12 09:29:35 -0600

    The routes and the controllers were created and used on the index file.

--- api-rest-social-network/index.js
+++ api-rest-social-network/index.js
@@ -5,12 +5,20 @@ const cors = require("cors");
 const PORT = 3900;
 const app = express();
 
+const routerFollow = require("./routes/follow");
+const routerUser = require("./routes/user");
+const routerPublication = require("./routes/publication");
+
 connection();
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/follow", routerFollow);
+app.use("/users", routerUser);
+app.use("/publications", routerPublication);
 
 app.listen(PORT, () => {
   console.log(`✅ Server listen at port: ${PORT}`);
